f970da7646e5a3bb69000c7156e40395aba74132
Raul Argott <raulargott@hotmail.com>
2020-05-23 20:56:58 -0500

    dashboard

---
+++ app/Widgets/ProductDimmer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Widgets;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+
+class ProductDimmer extends \TCG\Voyager\Widgets\BaseDimmer
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = \App\Product::count();
+        $string = 'Products';
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-bag',
+            'title'  => "{$count} {$string}",
+            'text'   => __('voyager::dimmer.user_text', ['count' => $count, 'string' => Str::lower($string)]),
+            'button' => [
+                'text' => 'Products',
+                'link' => route('voyager.products.index'),
+            ],
+            'image' => '/products-bg.jpg',
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return Auth::user()->can('browse', Voyager::model('User'));
+    }
+}

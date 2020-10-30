2e82d5f4c0a67f8f9b308432bd0c97bf091ee433
Raul Argott <raulargott@hotmail.com>
2020-10-30 14:19:24 -0600

    test pull

--- resources/views/POS/show.blade.php
+++ resources/views/POS/show.blade.php
@@ -40,7 +40,7 @@
     <div class="cart-items">
     <div class="cart-row">
             <div class="cart-item cart-column">
-                <img class="cart-item-image" src="" width="100" height="100">
+                
                 <span class="cart-item-title">T-Shirt blue</span>
             </div>
             <span class="cart-price cart-column">$29.99</span>
